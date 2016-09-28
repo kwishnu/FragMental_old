50ae6c493efbec75ef43d37e9df1e4bbabcf9554
kwishnu <kwish1777@gmail.com>
2016-09-27 18:59:41 -0700

    Test

--- game.js
+++ game.js
@@ -3,11 +3,11 @@ import { StyleSheet, Text, View, TouchableHighlight } from 'react-native';
 
 //var FragTiles = require('./frag_tiles.js');
 var {width, height} = require('Dimensions').get('window');
-var NUM_HIGH = 4;
 var NUM_WIDE = 5;
+var NUM_HIGH = 4;
 var CELL_WIDTH = Math.floor(width * .2); // 20% of the screen width
-var CELL_HEIGHT = CELL_WIDTH * 0.6; // 20% of the screen width
-var CELL_PADDING = Math.floor(CELL_WIDTH * .05); // 5% of the cell size
+var CELL_HEIGHT = CELL_WIDTH * 0.6;
+var CELL_PADDING = Math.floor(CELL_WIDTH * .05); // 5% of the cell width
 var BORDER_RADIUS = CELL_PADDING * 0.6;
 var TILE_SIZE = CELL_WIDTH - CELL_PADDING * 2;
 var LETTER_SIZE = Math.floor(TILE_SIZE * .4);
@@ -32,29 +32,30 @@ class Game extends React.Component{
 
   drawTiles() {
     var result = [];
+    for (var row = 0; row < NUM_WIDE; row++) {
+        for (var col=0; col<NUM_HIGH; col++){
+              var key = row * NUM_WIDE + col;
+              var style = {
+                left: col * CELL_WIDTH + CELL_PADDING,
+                top: row * CELL_WIDTH + CELL_PADDING,
+                }
+                result.push(this.drawTile(key, style));
 
-    for (var i=0; i<4; i++){
-          var id = i;
-          var style = {
-            left: i * CELL_WIDTH + CELL_PADDING,
-            top: i * CELL_WIDTH + CELL_PADDING,
-            }
-            result.push(this.drawTile(id, style));
-
+        }
     }
     return result;
 
   }
 
 
-  drawTile(id, style) {
-    return <View key={id}>
-        <TouchableHighlight  style={styles.tile} underlayColor='transparent' onPress={() => this.show()}><Text style={styles.letter}>{this.state.theWord}</Text></TouchableHighlight>
+  drawTile(key, position) {
+    return <View style={[styles.tile, position]} key={key}>
+        <TouchableHighlight   underlayColor='transparent' onPress={() => this.show(key)}><Text style={styles.letter}>{key}</Text></TouchableHighlight>
                       </View>
   }
 
-  show(){
-  window.alert(this.state.text);
+  show(which){
+  window.alert(which);
   }
 
 };
@@ -78,6 +79,7 @@ var styles = StyleSheet.create({
     backgroundColor: 'transparent',
   },
   tile: {
+   position: 'absolute',
     width: CELL_WIDTH,
     height: CELL_HEIGHT,
     borderRadius: BORDER_RADIUS,
