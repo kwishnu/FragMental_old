87d156d53e352d890ecaec08e52bc0e1b88dae44
kwishnu <kwish1777@gmail.com>
2016-09-27 23:59:13 -0700

    Board Grid roughed out

--- game.js
+++ game.js
@@ -5,14 +5,13 @@ import { StyleSheet, Text, View, TouchableHighlight } from 'react-native';
 var {width, height} = require('Dimensions').get('window');
 var NUM_WIDE = 5;
 var NUM_HIGH = 4;
-var CELL_WIDTH = Math.floor(width * .2); // 20% of the screen width
+var CELL_WIDTH = Math.floor(width * .17); // 20% of the screen width
 var CELL_HEIGHT = CELL_WIDTH * 0.6;
 var CELL_PADDING = Math.floor(CELL_WIDTH * .05); // 5% of the cell width
 var BORDER_RADIUS = CELL_PADDING * 0.6;
-var TILE_SIZE = CELL_WIDTH - CELL_PADDING * 2;
-var LETTER_SIZE = Math.floor(TILE_SIZE * .4);
-
-
+var TILE_WIDTH = CELL_WIDTH - CELL_PADDING * 2;
+var TILE_HEIGHT = CELL_HEIGHT - CELL_PADDING * 2;
+var LETTER_SIZE = Math.floor(TILE_HEIGHT * .6);
 
 class Game extends React.Component{
   constructor(props) {
@@ -24,46 +23,37 @@ class Game extends React.Component{
   }
   render() {
     return (
-        <View style={styles.container}>
+        <View style={styles.container}><View style={styles.tiles_container}>
             {this.drawTiles()}
-        </View>
+        </View></View>
     );
   }
 
   drawTiles() {
     var result = [];
-    for (var row = 0; row < NUM_WIDE; row++) {
-        for (var col=0; col<NUM_HIGH; col++){
+    for (var row = 0; row < NUM_HIGH; row++) {
+        for (var col=0; col<NUM_WIDE; col++){
               var key = row * NUM_WIDE + col;
               var style = {
                 left: col * CELL_WIDTH + CELL_PADDING,
-                top: row * CELL_WIDTH + CELL_PADDING,
+                top: row * CELL_HEIGHT + CELL_PADDING,
                 }
                 result.push(this.drawTile(key, style));
-
         }
     }
     return result;
-
   }
-
 
   drawTile(key, position) {
-    return <View style={[styles.tile, position]} key={key}>
-        <TouchableHighlight   underlayColor='transparent' onPress={() => this.show(key)}><Text style={styles.letter}>{key}</Text></TouchableHighlight>
-                      </View>
+    return <View  key={key}>
+             <TouchableHighlight  style={[styles.tile, position]} underlayColor='#0F0'
+             onPress={() => this.show(key)}><Text style={styles.letter}>{key}</Text></TouchableHighlight>
+           </View>
   }
-
   show(which){
   window.alert(which);
   }
-
 };
-
-
-
-
-
 
 
 var styles = StyleSheet.create({
@@ -80,17 +70,17 @@ var styles = StyleSheet.create({
   },
   tile: {
    position: 'absolute',
-    width: CELL_WIDTH,
-    height: CELL_HEIGHT,
+    width: TILE_WIDTH,
+    height: TILE_HEIGHT,
     borderRadius: BORDER_RADIUS,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#f0f0ff',
+    backgroundColor: '#dedffa',
   },
-
   letter: {
     color: '#00f',
     fontSize: LETTER_SIZE,
+    backgroundColor: 'transparent',
   },
 });
 
